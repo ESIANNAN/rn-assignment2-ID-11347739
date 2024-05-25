a134e4ad3fea582598f90010db322b0452313415
Philomina Annan- 11347739 <pannan027@st.ug.edu.gh>
2024-05-25 19:55:27 +0000

    Update App.js
    changed the text of "my name is Annan Philomina " to "My name is Philomina"

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Annan Philomina</Text>
+      <Text style={styles.text}>My name is Philomina</Text>
       <StatusBar style="auto" />
     </View>
   );

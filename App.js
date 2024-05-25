628ba033999f61167076b5726b93e748f450da2d
Philomina Annan- 11347739 <pannan027@st.ug.edu.gh>
2024-05-25 19:57:57 +0000

    Update App.js
    I made the text Philomina bold

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Philomina</Text>
+      <Text style={styles.text}>My name is <b>Philomina</b></Text>
       <StatusBar style="auto" />
     </View>
   );

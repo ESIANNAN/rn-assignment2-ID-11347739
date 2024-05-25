91dae08d6afffeecae528fa418b99826fb06ce64
Philomina Annan- 11347739 <pannan027@st.ug.edu.gh>
2024-05-25 20:10:25 +0000

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is <b>Philomina</b></Text>
+      <Text style={styles.text}>My name is <b>Philomina </Text>
       <StatusBar style="auto" />
     </View>
   );

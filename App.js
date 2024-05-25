a7ec4fac98aa15740e53fef3ab7b01137753c582
Philomina Annan- 11347739 <pannan027@st.ug.edu.gh>
2024-05-25 17:42:14 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My name is Annan Philomina</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FF0000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '24',
+  },
+});
